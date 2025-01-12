cefcfabdcbe92a8f573e0bbd28dfd0868a406549
Aditya <adityapermono8@gmail.com>
2025-01-12 16:25:36 +0700

    menambahkan class data MahasiswaEvent

--- app/src/main/java/com/example/p13firebase/ui/insert/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/p13firebase/ui/insert/viewmodel/InsertViewModel.kt
@@ -7,3 +7,12 @@ class InsertViewModel (
     private val mhs: RepositoryMhs
 ): ViewModel() {
 }
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
