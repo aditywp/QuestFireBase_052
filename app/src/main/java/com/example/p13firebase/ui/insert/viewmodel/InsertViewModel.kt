b519f914e1348fb8be4f3e8ab2c36b7773660e3b
Aditya <adityapermono8@gmail.com>
2025-01-12 16:29:28 +0700

    menambahkan fungsi MahasiswaEvent untuk mengubah MahasiswaEvent ke Mahasiswa

--- app/src/main/java/com/example/p13firebase/ui/insert/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/p13firebase/ui/insert/viewmodel/InsertViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.p13firebase.ui.insert.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.p13firebase.model.Mahasiswa
 import com.example.p13firebase.repository.RepositoryMhs
 
 class InsertViewModel (
@@ -15,4 +16,13 @@ data class MahasiswaEvent(
     val alamat: String = "",
     val kelas: String = "",
     val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
 )
