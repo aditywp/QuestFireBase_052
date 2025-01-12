d5e565f79aeb08acf589e8552d944c80b150647c
Aditya <adityapermono8@gmail.com>
2025-01-12 16:50:39 +0700

    memperbaiki kurung kurawal dan menambahkan inisialisasi InsertViewModel

--- app/src/main/java/com/example/p13firebase/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/p13firebase/ui/PenyediaViewModel.kt
@@ -6,12 +6,19 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.p13firebase.MahasiswaApp
 import com.example.p13firebase.ui.home.viewmodel.HomeViewModel
+import com.example.p13firebase.ui.insert.viewmodel.InsertViewModel
 
-object PenyediaViewModel{
+object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(
-                MahasiswaApp().container.repositoryMhs)
+                MahasiswaApp().container.repositoryMhs
+            )
+        }
+        initializer {
+            InsertViewModel(
+                MahasiswaApp().container.repositoryMhs
+            )
         }
     }
 }
